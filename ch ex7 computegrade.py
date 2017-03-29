8a618b49c0fb986c388e7d3111a62e3b814a5258
Timothy Yu <tyudraft@gmail.com>
2017-03-29 11:50:59 -0400

    computegrade: rewrite of code into function -> computegrade()

--- ch ex7 computegrade.py
+++ ch ex7 computegrade.py
@@ -1,16 +1,26 @@
-score = input("Enter Score:")
-score = float(score)
+# Excercise 4, Ch. 4 Pg. 55, Python For Informatics v3
 
-if score > 1.0 :
-    print ('Score out of range')
-    exit()
-elif score >= 0.9 :
-    print ('A')
-elif score >= 0.8 :
-    print ('B')
-elif score >= 0.7 :
-    print ('C')
-elif score >= 0.6 :
-    print ('D')
-elif score < 0.6 :
-    print ('F')
+# Rewrite the grade program from the previous chapter using a function
+# called computegrade that takes a score as its parameter and returns
+# a grade as a string
+
+score = input("Enter a numeric grade from 0 to 100: ")
+
+def computegrade(score):
+    score = float(score) / 100
+    if score > 1.0 :
+        return ('Score out of range')
+        exit()
+    elif score >= 0.9 :
+        return ('A')
+    elif score >= 0.8 :
+        return ('B')
+    elif score >= 0.7 :
+        return ('C')
+    elif score >= 0.6 :
+        return ('D')
+    elif score < 0.6 :
+        return ('F')
+
+print (computegrade(score))
+
