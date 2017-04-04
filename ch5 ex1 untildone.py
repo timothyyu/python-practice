811ed46a4753122f2032b9d93efa7cfc2c32b221
Timothy Yu <tyudraft@gmail.com>
2017-04-03 20:55:20 -0400

    added first version of ex1, ch.5, p.55

---
+++ ch5 ex1 untildone.py
@@ -0,0 +1,23 @@
+# Excercise 1, Ch. 5 Pg. 65, Python For Informatics v3
+
+# Write a program which repeatedly reads numbers until the user enters
+# “done”. Once “done” is entered, print out the total, count, and average of the
+# numbers. If the user enters anything other than a number, detect their mistake
+# using try and except and print an error message and skip to the next number.
+
+lst = []
+inputloop = True
+while inputloop is True:
+    try:
+        number=input("Enter a number:")
+        if number == "done":
+            break
+        number = int(number)
+        lst.append(number)
+    except ValueError:
+        print("Invalid input")
+        continue
+avg = sum(lst) / len(lst)
+
+print (sum(lst),len(lst), avg)
+
