2a105d09eae89bc876cdc00126420d929889984d
Timothy Yu <tyudraft@gmail.com>
2017-05-11 14:34:50 -0400

    ch6 ex3 countfunc comment header/info added

--- ch6 ex3 countfunc.py
+++ ch6 ex3 countfunc.py
@@ -1,8 +1,12 @@
+# Excercise 6, Ch. 6 Pg. 70, Python For Informatics v3
+#Encapsulate a looping program that counts the number of times
+# a specified letter appears in a string into a function (and accepts the string and letter as arguments)
+
 def count(str, let):
     count = 0
     for letter in str:
         if letter == let:
-            count = count +1
+            count = count + 1
     print(count)
 
 count("banana",'a')
