1bf9f7629736f243b5aad568b0fe76c827379a95
Timothy Yu <tyudraft@gmail.com>
2017-05-11 14:47:38 -0400

    ch6 ex3 countfunc ['a' in b] not working, but method call for count is working

--- ch6 ex4 countmethod.py
+++ ch6 ex4 countmethod.py
@@ -4,3 +4,7 @@
 # in the previous exercise. Read the documentation of this method at
 # https://docs.python.org/3.5/library/stdtypes.html#string-methods and write an
 # invocation that counts the number of times the letter a occurs in “banana”
+
+b = "banana"
+b.count('a')
+'a' in b
