14c5ddd68c6823971f9d3b2bb8f9b0674d0432b6
Timothy Yu <tmichael.yu@gmail.com>
2019-01-03 17:25:08 -0500

    ex7.py added

---
+++ ex7.py
@@ -0,0 +1,28 @@
+#ex7 p.57
+
+print("Mary had a little lamb.")
+
+# print statement format with {} and .format
+	# string with word 'snow' in it - not assigned to variable in this case
+print("Its fleece was white as {}.".format('snow'))
+print("and everywhere that Mary went.")
+
+# print "." ten times consecutively
+print("." * 10)
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# concat end characters and add and space between first and second word output 
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
