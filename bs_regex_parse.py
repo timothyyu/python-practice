104a70d0a1169b9389e01ee8a48fd975337f24cc
Timothy Yu <tmichael.yu@gmail.com>
2019-01-04 16:56:10 -0500

    bs_regex_parse.py added

---
+++ bs_regex_parse.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as Soup
+import re
+
+string = '<p>Please click <a href="http://www.dr-chuck.com">here</a></p>'
+match = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', string)
+
+html = Soup(string, 'html.parser')    
+bsm=  [a['href'] for a in html.find_all('a')]
+match2 = re.findall('"http.*.*"',string)
+
+print(match)
+print(match2)
+print(bsm)
