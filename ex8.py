7cdd0fcd17833c38e7d4d6fd305db6dbf3c78e3d
Timothy Yu <tmichael.yu@gmail.com>
2019-01-04 18:16:19 -0500

    ex8.py formatter.format() exercise

---
+++ ex8.py
@@ -0,0 +1,17 @@
+#ex 8 p. 60-61
+
+formatter = " {} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
+
+# Excercise is using a function to turn the formatter variable
+# into other strings
