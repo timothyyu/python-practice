a971f970e403e07bb2e6778adf53643f757885d7
Timothy Yu <tmichael.yu@gmail.com>
2019-01-08 11:19:35 -0500

    add ex9, ex10 python

---
+++ ex10.py
@@ -0,0 +1,26 @@
+#p.64 ex10
+
+# Escape sequence 	
+	# escape double-quotes and single-quotes so python knows to include them in the string
+edqis = "I am 6'2\" tall." # escape double-quote inside string
+
+esqis = 'I am 6\'2" tall.' # escape single-quote inside string
+
+print(edqis)
+print(esqis)
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
---
+++ ex9.py
@@ -0,0 +1,19 @@
+#p.62 ex9
+
+days = "Mon Tue Wed Thru Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+
+#Start months on new line 
+	#"\nJan\"Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# \n ----> new line character
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we ant, or 5, or 6.
+""")
