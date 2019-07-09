357c01435495e4ecb3b914dd884ce03f3dc43e9d
Timothy Yu <tmichael.yu@gmail.com>
2019-07-09 18:33:47 -0400

    ex12 & ex13

---
+++ lpthw/ex12.py
@@ -0,0 +1,7 @@
+#p68-69 ex 11
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} lbs heavy.")
