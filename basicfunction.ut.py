98a004819bea296d9c0ace818c10b6762b3eaade
Timothy Yu <tmichael.yu@gmail.com>
2019-11-08 17:15:27 -0500

    basicfunction.py and ut update

--- basicfunction.ut.py
+++ basicfunction.ut.py
@@ -6,20 +6,35 @@ from basicfunction import BasicFunction
 # import the class of interest (BasicFunction) from the basicfunction.py
 # file
  
+
+# There's no explicit way to cause a test to "pass"
+    # A test's status depends on the presence or absence of an exception
 class TestBasicFunction(unittest.TestCase):
     def setUp(self):
-        # setup() method to create a local instance of the class
+        # setUp() method to create a local instance of the class
+
+        # setUp() also used to establish test fixtures for all tests, i.e.
+        # resources/libaries/connections and imports needed for said tests
+            # setUp() to configure fixtures
+            # tearDown() to clean up fixtures at end of unit test
         self.func = BasicFunction()
+
+    # def test_fail(self):
+    #     # desired outcome produces result that can be evaluated as false ---> use assertFalse()
+    #     self.assertTrue(False,'failure message goes here - assertTrue is False for test_fail()')
  
-    def test_1(self):
-        self.assertTrue(True)
+    # def test_1(self):
+    #     self.assertTrue(True)
  
     def test_2(self):
-        self.assertTrue(True)
- 
+        # Case to check that class object state assignment is mutable with None assignment
+        # upon new initialization of class obj
+        self.func.state = None 
+        self.assertIsNone(self.func.state,None)
+
     def test_3(self):
         # unit test to check for initialization of local class
-        self.assertEqual(self.func.state, 0)
+        self.assertEqual(self.func.state, 0,'0-state init check - pass')
  
     def test_4(self):
         self.func.increment_state()
