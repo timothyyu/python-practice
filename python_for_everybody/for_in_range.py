f978b71ce147b6e9058372929f2666c2e67d0741
Timothy <tmichael.yu@gmail.com>
2020-07-16 20:47:49 -0400

    p4e/four_in_range.py + nn_percep folder

---
+++ python_for_everybody/for_in_range.py
@@ -0,0 +1,11 @@
+x = 0
+for y in range(5):
+    print(x,y)
+    x = x + y
+    # x = 0 + 0
+    # x = 0 + 1 ==> x = 1
+    # x = 1 + 2 ==> x = 3
+    # x = 3 + 3 ==> x = 6
+    # x = 6 + 4 ==> x = 10 
+print(x)
+
